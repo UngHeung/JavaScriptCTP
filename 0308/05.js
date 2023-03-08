4c9f6c0ee65f72cda091a738437966e12e1a904f
UngHeung <t01047377477@gmail.com>
2023-03-08 21:01:38 +0900

    5번 문제

---
+++ 0308/05.js
@@ -0,0 +1,13 @@
+// 05. 양꼬치
+
+function solution(n, k) {
+    let answer = n * 12000;
+
+    if (n >= 10) {
+        k -= Math.floor(n / 10);
+    }
+
+    answer += k * 2000;
+
+    return answer;
+}
