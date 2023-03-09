db45da5d169ec10d93eeae6b548ae40e9b1c280a
UngHeung <t01047377477@gmail.com>
2023-03-09 18:35:19 +0900

    8번 문제

---
+++ 0309/08.js
@@ -0,0 +1,13 @@
+// 08. 중복된 숫자 개수
+
+function solution(array, n) {
+    let answer = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === n) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
