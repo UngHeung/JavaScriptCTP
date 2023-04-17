11018cd5c9051c4467b12abc132c91e129548401
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-04-17 11:39:16 +0900

    [level 0] Title: 369게임, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,12 +1,4 @@
 function solution(order) {
-    let answer = 0;
-    
-    order+='';
-    for (let i = 0; i < order.length; i++) {
-        if (order.charAt(i) === '3' || order.charAt(i) === '6' || order.charAt(i) === '9') {
-            answer++;
-        }
-    }
-    
+    let answer = String(order).split(/[369]/g).length-1;
     return answer;
 }
