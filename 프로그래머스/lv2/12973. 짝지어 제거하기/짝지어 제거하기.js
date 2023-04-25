4a6a74d5a650ac253c4e553e6389095790858984
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-04-26 02:19:59 +0900

    [level 2] Title: 짝지어 제거하기, Time: 38.42 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,17 +1,9 @@
 function solution(s) {
     const stack = [];
-
-    for (let i = 0; i < s.length; i++) {
-        if (stack.length === 0) {
-            stack.push(s[i]);
-        } else {
-            if (stack[stack.length-1] === s[i]) {
-                stack.pop();
-            } else {
-                stack.push(s[i]);
-            }
-        }
+    for (v of s) {
+        stack.length===0?stack.push(v)
+        :stack[stack.length-1]===v?stack.pop()
+        :stack.push(v);
     }
-    
-    return stack.length === 0 ? 1 : 0;
+    return stack.length===0?1:0;
 }
