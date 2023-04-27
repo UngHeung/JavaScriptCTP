df8758b308ca799763a90ea874499aa554b7c781
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-04-28 02:36:16 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,9 +1,3 @@
 function solution(a, b) {
-    if (a % 2 && b % 2) {
-        return a**2 + b**2;
-    } else if (a % 2 || b % 2) {
-        return (a + b) * 2;
-    } else {
-        return Math.abs(a - b);
-    }
+    return a%2&&b%2?a**2+b**2:a%2||b%2?(a+b)*2:Math.abs(a-b)
 }
