47b5a4a9ee700b6d097afa114dba22838701b8d8
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-04-28 03:11:30 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, k) {
-    const answer = []
-    for(let i = 0; i < k; i++) {
-        answer.push(my_string)
-    }
-    return answer.join('')
+    return my_string.repeat(k);
 }
