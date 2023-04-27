a33a0132aaf37506840cd7c29d4913868636f9a6
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-04-27 17:25:38 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = count1(n);
+    while (true) {
+        n++;
+        if (count1(n) === answer) {
+            answer = n;
+            break;
+        }
+    }
+    return answer;
+}
+
+function count1(number) {
+    const bin = number.toString(2);
+    return bin.length - bin.replaceAll("1", "").length;
+}
