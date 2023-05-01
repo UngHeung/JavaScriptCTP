efe5c8a40801b0074d7b6d862abf6f65db805908
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-05-01 20:15:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,1 +1,2 @@
-const solution=(pn)=>[...Array(pn.length-4).fill('*'),pn.slice(-4)].join('')
+// const solution=(pn)=>[...Array(pn.length-4).fill('*'),pn.slice(-4)].join('')
+const solution=(p)=>[...p].fill('*',0,p.length-4).join('')
