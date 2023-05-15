dafd169502bb7726295cf8c26eaef0b488d55b88
ungHeung <108179643+UngHeung@users.noreply.github.com>
2023-05-15 23:38:42 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,9 @@
+function solution(d, budget) {
+    let answer = 0
+    return d.sort((a, b) => a - b).reduce((a, b, i = 1) => {
+        if (a <= budget) {
+            answer = i
+        }
+        return a + b
+    })>budget?answer:answer+1
+}
